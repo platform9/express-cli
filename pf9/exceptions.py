2d537181940700936b94235b9fea4767a281b3fc
Supreeth Shivanand <supreeth90@gmail.com>
2020-09-02 14:28:26 -0700

    PMK-3133: Fixing cluster prep-node failure leads to exception in send_track_error

--- pf9/exceptions.py
+++ pf9/exceptions.py
@@ -11,6 +11,8 @@ class CLIException(Exception):
     def __repr__(self):
         return self.msg
 
+    def __str__(self):
+        return self.msg
 
 class DUCommFailure(CLIException):
     def __init__(self, msg):
